8bd130bd3bdd679617a5e85a5aa01612d50e8340
davinci1997 <71709014+davinci1997@users.noreply.github.com>
2020-11-10 20:18:51 +0100

    versuch bild zu inkludieren (gescheitert)

--- Galerie/Galerieprojekt/main.py
+++ Galerie/Galerieprojekt/main.py
@@ -1,25 +1,24 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask ("Galerie")
+app = Flask ("Galerie",)
 
+@app.route("/",)
+def lazy ():
+    return render_template("index.html")
 
-@app.route("/index")
+@app.route("/index", methods=["GET", "POST"])
 def index ():
     return render_template("index.html")
 
 
 @app.route("/eingabe", methods= ["GET","POST"])
 def eingabe ():
-
-
     return render_template("eingabe.html", app_name="Galerie - eingabe")
 
 
 @app.route("/beispiel", methods= ["GET","POST"])
 def beispiel ():
-
-
     return render_template("beispiel.html", app_name="Galerie - beispiel")
 
 if __name__ == "__main__":
